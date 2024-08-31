3c3f88e2645f0cc2bb9d348d64f2a779db230c9b
othman El Hadrati <othmanelhadrati@gmail.com>
2024-08-31 18:14:34 +0100

    update

--- src/Components/SkillsPart.jsx
+++ src/Components/SkillsPart.jsx
@@ -6,8 +6,10 @@ import { TbBrandFramerMotion } from 'react-icons/tb';
 import { RiTailwindCssFill } from 'react-icons/ri';
 import { SiFlask, SiOracle, SiMysql } from 'react-icons/si';
 import { DiGit, DiGithubBadge, DiLinux, DiBootstrap } from 'react-icons/di';
+import { BiLogoPostgresql } from 'react-icons/bi';
 import { SiFastapi } from 'react-icons/si';
 import { SiShadcnui } from 'react-icons/si';
+import { SiPostman } from 'react-icons/si';
 
 import { motion } from 'framer-motion';
 import CIcon from './Ccompo';
@@ -33,21 +35,25 @@ const SkillCategory = ({ category, skills }) => (
           >
             <skill.icon />
 
-            <motion.div 
-              className="absolute font-bold -top-1 w-7 h-9 flex justify-start flex-col items-center"
-              initial={{ opacity: 0, y: 0 }}
-              whileHover={{ opacity: 1, y: -19, height: 53 }}
+            <motion.div
+              className="absolute group font-bold -top-1 w-7 h-9 flex justify-start flex-col items-center"
+              initial={{ opacity: 1, y: 0 }}
+              whileHover={{
+                opacity: 1,
+                y: -19,
+                height: 53,
+              }}
               transition={{
-                duration: 0.3,
+                duration: 0.35,
                 ease: 'easeInOut',
                 type: 'spring',
                 stiffness: 60,
               }}
             >
-              <div className="px-3 w-max h-6 pt-0.5 rounded-md bg-white text-sm text-primary4 shadow-lg">
+              <div className="px-3 group-hover:block hidden w-max h-6 pt-0.5 rounded-md bg-white text-sm text-primary4 shadow-lg">
                 {skill.name}
               </div>
-              <div className="w-0 h-0 border-l-4 border-l-transparent border-r-4 border-r-transparent border-t-4"></div>
+              <div className="w-0 h-0 group-hover:block hidden border-l-4 border-l-transparent border-r-4 border-r-transparent border-t-4" />
             </motion.div>
           </motion.div>
         ))}
@@ -83,24 +89,27 @@ export default function SkillsPart() {
         ]}
       />
       <SkillCategory
+        category="Databases ( SQL / PlSql )"
+        skills={[
+          { icon: SiOracle, name: 'Oracle' },
+          { icon: SiMysql, name: 'MySQL' },
+          { icon: BiLogoPostgresql, name: 'PostgreSQL' },
+        ]}
+      />
+      <SkillCategory
         category="Operating Systems"
         skills={[
           { icon: DiLinux, name: 'Linux' },
           { icon: FaWindows, name: 'Windows' },
         ]}
       />
+
       <SkillCategory
-        category="Databases ( SQL / PlSql )"
-        skills={[
-          { icon: SiOracle, name: 'Oracle' },
-          { icon: SiMysql, name: 'MySQL' },
-        ]}
-      />
-      <SkillCategory
-        category="Version Control"
+        category="Version Control & Tools"
         skills={[
           { icon: DiGit, name: 'Git' },
           { icon: DiGithubBadge, name: 'GitHub' },
+          { icon: SiPostman, name: 'Postman' },
         ]}
       />
     </div>
